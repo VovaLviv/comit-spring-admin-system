4aa15c88c8e6e437cbb71fcc16f50f8aa83367f8
Vova <83168499+VovaLviv@users.noreply.github.com>
2023-12-01 20:12:05 -0400

    first commit

---
+++ src/main/java/org/comit/spring/ComitSpringAdminSystemApplication.java
@@ -0,0 +1,13 @@
+package org.comit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComitSpringAdminSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComitSpringAdminSystemApplication.class, args);
+	}
+
+}
